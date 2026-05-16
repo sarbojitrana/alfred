2123024a8261fb3836f78ebe863ae9c19e023b09
sarbojitrana <sarbojitrana47c@gmail.com>
2026-05-16 21:41:35 +0530

    add dto

---
+++ apps/backend/internal/model/comment/comment.go
@@ -0,0 +1,15 @@
+package comment
+
+import (
+	"github.com/google/uuid"
+	"github.com/sarbojitrana/go-alfred/internal/model"
+)
+
+
+
+type Comment struct{
+	model.Base
+	TodoID		uuid.UUID 		`json:"todoId" db:"todo_id"`
+	UserID		string			`json:"userId" db:"user_id"`
+	Content		string			`json:"content" db:"content"`
+}
---
+++ apps/backend/internal/model/comment/dto.go
@@ -0,0 +1,56 @@
+package comment
+
+import (
+	"github.com/google/uuid"
+	"github.com/go-playground/validator/v10"
+)
+
+
+
+//-------------------------------------------------------------------------------------------
+type AddCreatePayload struct{
+	TodoId 			uuid.UUID			`param:"id" validate:"required,uuid"`
+	Content			string				`json:"content" validate:"required,min=1,max=1000"`
+}
+
+
+func (p *AddCreatePayload) Validate() error{
+	validate := validator.New()
+	return validate.Struct(p)
+}
+
+
+
+//-------------------------------------------------------------------------------------------
+
+type GetCommentsByTodoIDPayload struct {
+	TodoID uuid.UUID `param:"id" validate:"required,uuid"`
+}
+
+func (p *GetCommentsByTodoIDPayload) Validate() error {
+	validate := validator.New()
+	return validate.Struct(p)
+}
+
+// ------------------------------------------------------------
+
+type UpdateCommentPayload struct {
+	ID      uuid.UUID `param:"id" validate:"required,uuid"`
+	Content string    `json:"content" validate:"required,min=1,max=1000"`
+}
+
+func (p *UpdateCommentPayload) Validate() error {
+	validate := validator.New()
+	return validate.Struct(p)
+}
+
+// ------------------------------------------------------------
+
+type DeleteCommentPayload struct {
+	ID uuid.UUID `param:"id" validate:"required,uuid"`
+}
+
+func (p *DeleteCommentPayload) Validate() error {
+	validate := validator.New()
+	return validate.Struct(p)
+}
