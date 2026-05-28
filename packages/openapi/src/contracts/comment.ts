9ced1e547907a55f4aeecea4a721dd13b2025b37
sarbojitrana <sarbojitrana47c@gmail.com>
2026-05-29 01:39:59 +0530

    complete openapi contract

---
+++ packages/openapi/src/contracts/comment.ts
@@ -0,0 +1,62 @@
+import { getSecurityMetadata } from "@/utils.js";
+import {ZTodoComment} from "@alfred/zod";
+import { initContract } from "@ts-rest/core";
+import z from "zod";
+
+
+const c = initContract();
+
+const metadata = getSecurityMetadata();
+
+export const commentContract = c.router(
+    {
+        addComment:{
+            summary: "Add comment to todo",
+            path: "/todo/:id/comments",
+            method: "POST",
+            body: ZTodoComment.pick({
+                content: true,
+            }),
+            responses:{
+                201: ZTodoComment,
+            },
+            metadata: metadata,
+        },
+
+        getCommentsByTodoId :{
+            summary: "Get comments for todo",
+            path: "/todo/:id/comments",
+            method: "GET",
+            responses: {
+                200: z.array(ZTodoComment),
+            },
+            metadata: metadata,
+        },
+
+        updateComment:{
+            summary: "Update comment",
+            path: "/comments/:id",
+            method: "PATCH",
+            body: ZTodoComment.pick({
+                continue: true,
+            }),
+            responses:{
+                200: ZTodoComment,
+            },
+            metadata: metadata
+        },
+
+        deleteComment:{
+            summary: "Delete comment",
+            path: "/comments/:id",
+            method: "DELETE",
+            responses:{
+                204: z.void(),
+            },
+            metadata: metadata,
+        },
+    },{
+        pathPrefix: "/v1"
+    }
+
+)
